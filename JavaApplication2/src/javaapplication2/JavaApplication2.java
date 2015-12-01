ec2027bcb289016e20891d0d7c3687b27da88f43
s.binder <s.binder@10.1.105.23>
2015-12-01 12:07:03 +0000

    first array added

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -16,6 +16,11 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         System.out.println("cheers!") ; 
+        
+        int [] firstArray = new int[100] ;
+        for (int i = 0 ; i < 100 ; i ++){
+           firstArray[i] = i ;
+        }
     }
     
 }
